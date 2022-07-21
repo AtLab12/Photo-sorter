3aac3ec4bc77d3380311a19244ab5bd39ea5e017
A <mikolajzawada@Mikolajs-MBP.net>
2022-07-21 21:37:00 +0200

    Refactoring, prepared for unified usage.

--- Location_personalization.py
+++ Location_personalization.py
@@ -3,27 +3,34 @@ import os
 from os import path
 import csv
 from enum import Enum
-import geopy.distance
-from config import config  # python file containing only dictoary with custom config
+from config import config  # python file containing dictionary with custom config
+from config import path_to_timeline_csv
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
 
-class Location_Type(Enum):
+class LocationType(Enum):
     City = 'city'
     Tourism = 'tourism'
     Muniplicity = 'municipality'
     Country = 'country'
     Zone = 'zone'
+    Neighbourhood = 'neighbourhood'
 
-test_master_path = "/Users/mikolajzawada/Documents/Photos_to_sort"
-
-
-def alocate_photo(file_date, year, origin_path, loc, lat, lon, city=None, country=None, tourism=None,
-                  municipality=None):
+def alocate_photo(file_date, year, origin_path, loc, lat, lon, city=None, country=None, tourism=None, municipality=None, neighbourhood=None, isVideo=False):
     country_name = get_country_name(country)
     parent_folder = None
-    file_name = origin_path.split("/")[-1]
+    path_components = origin_path.split("/")
+    file_name = path_components[-1]
+    path_components.pop()
+    path_components.pop()
+    path_components.pop(0)
+    master_path = ""
+
+    for comp in path_components:
+        master_path += "/"
+        master_path += comp
+
     loc_type = None
     name = None
 
@@ -33,59 +40,70 @@ def alocate_photo(file_date, year, origin_path, loc, lat, lon, city=None, countr
         if zone.contains(point):
             parent_folder = zone_name + "_" + year
             name = zone_name
-            loc_type = Location_Type.Zone
+            loc_type = LocationType.Zone
             break
 
     if city is not None:
         if city in config[country_name]:
-            print(city)
             parent_folder = city + "_" + year
-            loc_type = Location_Type.City
+            loc_type = LocationType.City
             name = city
-        else:
-            pass
-            # print("Unfamiliar city ", city)
 
     if parent_folder is None and tourism is not None:
         if tourism in config["Tourism"]:
             parent_folder = tourism + "_" + year
             name = tourism
-            loc_type = Location_Type.Tourism
-        else:
-            pass
-            # print("Unknown tourism")
+            loc_type = LocationType.Tourism
+
+    if parent_folder is None and neighbourhood is not None:
+        if neighbourhood in config["Tourism"]:
+            parent_folder = neighbourhood + "_" + year
+            name = neighbourhood
+            loc_type = LocationType.Neighbourhood
 
     if parent_folder is None and municipality is not None:
         if municipality in config[country_name]:
             parent_folder = municipality + "_" + year
             name = municipality
-            loc_type = Location_Type.Muniplicity
-        else:
-            pass
-            # print("Unknown municipality")
+            loc_type = LocationType.Muniplicity
 
     if parent_folder is None and country is not None:
         parent_folder = country_name + "_" + year
         name = country_name
-        loc_type = Location_Type.Country
+        loc_type = LocationType.Country
 
     mode = 0o777
-    master_path = os.path.join(test_master_path, str(year))
+    master_path = os.path.join(master_path, str(year))
 
     if parent_folder is not None:
         if not path.exists(master_path):
             os.mkdir(master_path, mode)
 
-        parent_path = os.path.join(master_path, parent_folder)
+        if isVideo:
+            parent_path = os.path.join(master_path, parent_folder)
+            full_parent_path = os.path.join(parent_path, "Video")
+        else:
+            parent_path = os.path.join(master_path, parent_folder)
 
         if not path.exists(parent_path):
             os.mkdir(parent_path, mode)
 
-        os.replace(origin_path, parent_path + "/" + file_name)
+        if isVideo and not path.exists(full_parent_path):
+            os.mkdir(full_parent_path, mode)
+
+        if isVideo:
+            os.replace(origin_path, full_parent_path + "/" + file_name)
+        else:
+            os.replace(origin_path, parent_path + "/" + file_name)
+
         add_photo_info(file_name, parent_path, year, name, file_date, loc_type, municipality, lon, lat)
 
+        # updating timeline file
+        update_timeline_file(file_date, file_name, name, loc_type.value, lat, lon)
+
     else:
-        unknown_master_path = os.path.join(test_master_path, "unknown")
+        print("unknown")
+        unknown_master_path = os.path.join(master_path, "unknown")
         if not path.exists(unknown_master_path):
             os.mkdir(unknown_master_path, mode)
 
@@ -96,7 +114,7 @@ def alocate_photo(file_date, year, origin_path, loc, lat, lon, city=None, countr
 
         os.replace(origin_path, un_parent_path + "/" + file_name)
 
-        with open(test_master_path + '/' + "sortLog.txt", 'a') as f:
+        with open(master_path + '/' + "sortLog.txt", 'a') as f:
             f.write("\n")
             f.write(file_name)
             f.write("\n")
@@ -113,9 +131,10 @@ def get_country_name(country_code) -> str:
     return name
 
 
-def add_photo_info(file_name, parent_path, year, name, full_date, type: Location_Type, municipality=None,
+def add_photo_info(file_name, parent_path, year, name, full_date, type: LocationType, municipality=None,
                    longitude=None, latitude=None):
     path_to_info = parent_path + '/' + "photoInfo.csv"
+    full_date = full_date.split(" ")[0]
     parameters = ['File_name', 'Year', 'Name', 'Municipality', 'Full_date', 'Latitude', 'Longitude', 'Type']
     if path.exists(path_to_info):
         with open(path_to_info, 'a') as f:
@@ -126,21 +145,6 @@ def add_photo_info(file_name, parent_path, year, name, full_date, type: Location
             writer = csv.writer(f)
             writer.writerow(parameters)
             writer.writerow([file_name, year, name, municipality, full_date, latitude, longitude, type.value])
-
-
-# delete if not end up using
-def average_distance_from_set(folder_path, latitude, longitude):
-    path_to_data_file = folder_path + "/" + 'photoInfo.csv'
-    if path.exists(path_to_data_file):
-        df = pd.read_csv(path_to_data_file)
-        df = df.reset_index()
-
-        for index, row in df.iterrows():
-            tmp_lat = row['Latitude']
-            tmp_lon = row['Longitude']
-            print(folder_path)
-            print(geopy.distance.geodesic((latitude, longitude), (tmp_lat, tmp_lon)).km)
-        pass
 
 
 def tourism_in_folder(path_a) -> (str, [str]):
@@ -157,3 +161,60 @@ def tourism_in_folder(path_a) -> (str, [str]):
                 result = True
                 tourist_files.append(filename)
     return result, tourist_files
+
+
+def update_timeline_file(data, file_name, location_name, location_type, lat, lon):
+    date = str(data.split(" ")[0]).split(":")
+    time = str(data.split(" ")[1]).split(":")
+    if path.exists(path_to_timeline_csv):
+        with open(path_to_timeline_csv, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(
+                [
+                    date[0],
+                    date[1],
+                    date[2],
+                    time[0],
+                    time[1],
+                    time[2],
+                    file_name,
+                    location_name,
+                    location_type,
+                    lat,
+                    lon
+                ]
+            )
+    else:
+        with open(path_to_timeline_csv, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(
+                [
+                    "Year",
+                    "Month",
+                    "Day",
+                    "Hour",
+                    "Minute",
+                    "Second",
+                    "File_name",
+                    "Location_name",
+                    "Location_type",
+                    "Latitude",
+                    "Longitude"
+                ]
+            )
+
+            writer.writerow(
+                [
+                    date[0],
+                    date[1],
+                    date[2],
+                    time[0],
+                    time[1],
+                    time[2],
+                    file_name,
+                    location_name,
+                    location_type,
+                    lat,
+                    lon
+                ]
+            )
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import sys
 from exif import Image
 from geopy.geocoders import Nominatim
+import geopy.exc as geoexc
 import os
 import Location_personalization as loc
 from tqdm import tqdm
 from os import path
 import subprocess
+import pandas as pd
+from time import sleep
+from random import randint
+from config import path_to_timeline_csv  # python file containing path to timeline file
 
-# test_path = "/Users/mikolajzawada/Documents/Photos_to_sort/Official_test"
-# test_path = "/Users/mikolajzawada/Documents/Photos_to_sort/iPhone_2"
-# test_path = "/Users/mikolajzawada/Documents/Photos_to_sort/Official_test_video"
-test_path = "/Users/mikolajzawada/Documents/Photos_to_sort/DJI_Video_test"
-# unsorted_folder = input("Please provide path to folder you want to sort: ")
-geolocator = Nominatim(user_agent="AtLabPhotosorter")
+input_path = str(input("Please provide path to folder you want to sort: "))
+
+while not path.exists(input_path):
+    print("\nPath does not exist, please try again.\n")
+    input_path = input("Please provide path to folder you want to sort: ")
+
+geolocator = Nominatim(user_agent="Your_choice")
 
 final_format = {}
 paths = []
@@ -34,8 +39,8 @@ def get_year_from_date(date) -> str:
 def run_sort():
     years = set(())
 
-    for filename in tqdm(os.listdir(test_path)):
-        f = os.path.join(test_path, filename)
+    for filename in tqdm(os.listdir(input_path)):
+        f = os.path.join(input_path, filename)
         if os.path.isfile(f):
             with open(f, 'rb') as src:
                 if src.name.endswith(".jpeg") or src.name.endswith(".JPG"):
@@ -49,16 +54,19 @@ def run_sort():
 
                             lat = decimal_coords(img.gps_latitude, img.gps_latitude_ref)
                             lon = decimal_coords(img.gps_longitude, img.gps_longitude_ref)
-
-                            # (lat, lon, date, year, file_name, parent_path):
-                            sort_with_lcoation_data(lat, lon, file_date, year, src.name)
+                            sort_with_location_data(lat, lon, file_date, year, src.name)
                         else:
-                            pass
-                        # move to unknown folder
-                        # print("no location data for ", src.name)
-                    else:
-                        pass
-                elif src.name.endswith(".mov"):
+                            if 'software' in tags:
+                                software = img.software
+                                # throw to lighroom folder that needs to be manually sorted
+                                # I assume if they were edited they are important ;)
+                                if 'Lightroom' in software:
+                                    ligthroom_path = os.path.join(loc.test_master_path, "Lightroom")
+                                    file_name = src.name.split("/")[-1]
+                                    if not path.exists(ligthroom_path):
+                                        os.mkdir(ligthroom_path, 0o777)
+                                    os.replace(src.name, ligthroom_path + "/" + file_name)
+                elif src.name.endswith(".mov") or src.name.endswith(".MP4"):
                     exe = "exiftool"
                     process = subprocess.Popen(
                         [exe, f],
@@ -66,14 +74,17 @@ def run_sort():
                         stderr=subprocess.STDOUT
                     )
                     data_from_exiftoll = get_data_from_exiftool(process.stdout)
-                    # lat, lon, file_date, year, file_name
-                    sort_with_lcoation_data(
+
+                    sort_with_location_data(
                         data_from_exiftoll[0],
                         data_from_exiftoll[1],
                         data_from_exiftoll[3],
                         data_from_exiftoll[2],
-                        src.name
+                        src.name,
+                        isVideo=True
                     )
+
+                # sorting srt files created by DJI drones
                 elif src.name.endswith(".SRT"):
                     lat = None
                     lon = None
@@ -85,44 +96,53 @@ def run_sort():
                                 data = line.split(" ")
                                 for index, sub_data in enumerate(data):
                                     if 'latitude' in sub_data:
-                                        lat = float(data[index+1][:-1])
+                                        lat = float(data[index + 1][:-1])
                                     elif 'longitude' in sub_data:
-                                        lon = float(data[index+1][:-1])
+                                        lon = float(data[index + 1][:-1])
                                 date = list(file_in)[master_index].split(" ")[0][:-1]
                                 year = date.split("-")[0]
                                 break
 
-                    sort_with_lcoation_data(
+                    sort_with_location_data(
                         lat,
                         lon,
                         date,
                         year,
-                        src.name
+                        src.name,
+                        isVideo=True
                     )
 
-                    sort_with_lcoation_data(
+                    sort_with_location_data(
                         lat,
                         lon,
                         date,
                         year,
-                        src.name.replace(".SRT", ".MP4")
+                        src.name.replace(".SRT", ".MP4"),
+                        isVideo=True
                     )
 
+    answer = input("Do you want to try sorting photos without location data, based on those already sorted? (Y/N)")
+
+    if answer in ["Y", "y", "yes", "a jakze"]:
+        # sort based on timeline csv
+        sort_based_on_timeline_file()
 
 
-def sort_with_lcoation_data(lat, lon, file_date, year, file_name):
+def sort_with_location_data(lat, lon, file_date, year, file_name, isVideo=False):
     city_key = 'city'
     country_key = 'country_code'
     tourism_key = 'tourism'
     municipality_key = 'municipality'
+    neighbourhood_key = 'neighbourhood'
 
     try:
         location = geolocator.reverse(str(lat) + "," + str(lon)).raw['address']
-    except:
-        # internet to slow or other error
-        # simply
+    except geoexc.GeocoderTimedOut:
+        sleep(randint(1 * 100, 200) / 100)
+        location = geolocator.reverse(str(lat) + "," + str(lon)).raw['address']
+    except Exception as e:
+        # write to log file
         return
-    # years.add(year)
 
     has_country_info = country_key in location.keys()
 
@@ -146,6 +166,11 @@ def sort_with_lcoation_data(lat, lon, file_date, year, file_name):
     else:
         municipality = None
 
+    if neighbourhood_key in location.keys():
+        neighbourhood = location[neighbourhood_key]
+    else:
+        neighbourhood = None
+
     loc.alocate_photo(
         file_date,
         year,
@@ -156,7 +181,9 @@ def sort_with_lcoation_data(lat, lon, file_date, year, file_name):
         city=city,
         country=country,
         tourism=tourism,
-        municipality=municipality
+        municipality=municipality,
+        neighbourhood=neighbourhood,
+        isVideo=isVideo
     )
 
 def get_data_from_exiftool(data):
@@ -164,6 +191,9 @@ def get_data_from_exiftool(data):
     lon = None
     year = None
     full_date = None
+    make = None
+    camera_model_name = None
+
     for result in data:
         data_line = str(result.strip())[2:]
         data_line_components = data_line.split(":")
@@ -180,7 +210,7 @@ def get_data_from_exiftool(data):
             else:
                 lon = decimal_coords((first, second, third), direct)
         if tag_name == 'MediaCreateDate':
-            year = data_line_components[1]
+            year = data_line_components[1].replace(" ", "")
             month = data_line_components[2]
             sub_comp = data_line_components[3].split(" ")
             day = sub_comp[0]
@@ -189,8 +219,93 @@ def get_data_from_exiftool(data):
             minute = data_line_components[4]
             second = data_line_components[5].replace("'", "")
 
-            full_date = year+":"+month+":"+day+" "+hour+":"+minute+":"+second
+            full_date = year + ":" + month + ":" + day + " " + hour + ":" + minute + ":" + second
+        if tag_name == 'Make':
+            make = data_line_components[1].split(" ")
+        if tag_name == 'Camera Model Name':
+            camera_model_name == data_line_components[1]
 
-    return lat, lon, year, full_date
+    return lat, lon, year, full_date, make, camera_model_name
+
+
+def sort_based_on_timeline_file():
+    print("Sorting based on timeline file ...")
+
+    for filename in tqdm(os.listdir(input_path)):
+        isVideo = False
+        f = os.path.join(input_path, filename)
+        file_date = None
+        with open(f, 'rb') as src:
+            if os.path.isfile(f):
+                if src.name.endswith(".jpeg") or src.name.endswith(".JPG"):
+                    img = Image(src)
+                    if img.has_exif:
+                        tags = img.list_all()
+                        file_date = img.datetime_original
+                elif src.name.endswith(".mov") or src.name.endswith(".MP4"):
+                    exe = "exiftool"
+                    process = subprocess.Popen(
+                        [exe, f],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT
+                    )
+                    data_from_exiftoll = get_data_from_exiftool(process.stdout)
+                    file_date = data_from_exiftoll[3]
+                    isVideo = True
+                elif src.name.endswith(".SRT"):
+                    with open(f) as file_in:
+                        file_date = list(file_in)[2].split(" ")[0]
+                        isVideo = True
+
+        correct_file_type = src.name.endswith(".jpeg") or src.name.endswith(".JPG") or src.name.endswith(".MP4")
+
+        if file_date is not None and correct_file_type:
+            # place file based on date
+            date = str(file_date.split(" ")[0]).split(":")
+            time = str(file_date.split(" ")[1]).split(":")
+            if path.exists(path_to_timeline_csv):
+                df = pd.read_csv(path_to_timeline_csv)
+                df_file_date = df.loc[
+                    (df['Day'] == int(date[2])) &
+                    (df['Month'] == int(date[1])) &
+                    (df['Year'] == int(date[0]))
+                    ]
+
+                if df_file_date.shape[0] > 0:
+                    # there was media made the same day
+                    assigned_coordinates = find_closest_media_same_day(df_file_date, date, time)
+                    lat = assigned_coordinates[0]
+                    lon = assigned_coordinates[1]
+
+                    sort_with_location_data(
+                        lat,
+                        lon,
+                        file_date,
+                        date[0],
+                        src.name,
+                        isVideo
+                    )
+
+
+# returns lat and lon of the closest (time wise) media found
+def find_closest_media_same_day(data_frame, date, time) -> (float, float):
+    hour_P = int(time[0])
+    minute_P = int(time[1])
+
+    hours = data_frame['Hour'].tolist()
+    it = map(lambda hour: int(hour), hours)
+    hours = list(it)
+    hour_found = min(hours, key=lambda x: abs(x - hour_P))
+
+    dataFrame = data_frame.loc[data_frame['Hour'] == hour_found]
+
+    minutes = dataFrame['Minute'].tolist()
+    it = map(lambda minute: int(minute), minutes)
+    minutes = list(it)
+    minute_found = min(minutes, key=lambda x: abs(x - minute_P))
+
+    df = dataFrame.loc[(dataFrame['Hour'] == hour_found) & (dataFrame['Minute'] == minute_found)]
+
+    return df.iloc[0]['Latitude'], df.iloc[0]['Longitude']
 
 run_sort()

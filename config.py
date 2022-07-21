a9afa4e084c8587e33052bcc6cf677778dd80639
A <mikolajzawada@Mikolajs-MBP.net>
2022-07-21 23:22:13 +0200

    Added usa national park service registry

---
+++ config.py
@@ -0,0 +1,138 @@
+config = {
+    "United States": [
+        "Los Angeles",
+        "San Francisco",
+        "New York",
+        "Las Vegas",
+        "Anchorage",
+        "Fairbanks",
+        "Whitter",
+        "Denver",
+        "Estes Park",
+        "Seattle"
+    ],
+    "Poland": [
+        "Tychy",
+        "Cielmice",
+        "Katowice",
+        "Ustroń",
+        "Warszawa",
+        "Dębki",
+        "Wrocław",
+        "Kraków",
+        "Lądek Zdrój",
+        "Wadowice",
+        "Andrychów",
+        "Lanckorona",
+        "Świnoujście",
+        "Raszyn",
+        "Busko",
+        "Karwia",
+        "Jastrzębia Góra",
+        "Biało Góra",
+        "Gdańsk",
+        "Borek Strzeliński",
+        "Cieszyn",
+        "Opole",
+        "Michałowice"
+    ],
+    "Italy": [
+        "Venice"
+    ],
+    "Netherlands": [
+        "Wassenaar",
+        "Amsterdam",
+        "Katwijk",
+        "Den Haag",
+        "Rotterdam",
+        "Leiden"
+    ],
+    "Austria": [
+        "Vien"
+    ],
+    "United Kingdom": [
+        "London",
+        "Cambridge",
+        "Baldock"
+    ],
+    "Norway": [
+        "Ulvik",
+        "Oslo",
+        "Bergen"
+    ],
+    "Tourism": [
+        "Duinrell",
+        "Universal City"
+    ],
+    "Sweden": [
+
+    ],
+    "Switzerland": [
+        "Derborance"
+    ],
+    "Canada": [
+        "Toronto",
+        "Vancouver"
+    ],
+    "Germany": [
+
+    ],
+    "Czech Republic": [
+        "Praha"
+    ],
+    "Zones": {
+        "STO": [
+            (50.119722, 18.994278),
+            (50.121171, 18.996027),
+            (50.122492, 18.993293),
+            (50.121550, 18.991791)
+        ],
+        "Duinrell": [
+            (52.141061, 4.384915),
+            (52.143017, 4.387190),
+            (52.144045, 4.385978),
+            (52.146940, 4.388974),
+            (52.149014, 4.389982),
+            (52.152674, 4.384360),
+            (52.145181, 4.368206)
+        ],
+        "Derborance": [
+            (46.238090, 7.270396),
+            (46.282045, 7.238677),
+            (46.293677, 7.262521),
+            (46.297461, 7.258807),
+            (46.300855, 7.217750),
+            (46.288731, 7.203867),
+            (46.276395, 7.208342),
+            (46.271443, 7.236117),
+            (46.226446, 7.254224)
+        ],
+        "Bałtycka": [
+            (50.226392, 18.934003),
+            (50.222267, 18.933231),
+            (50.221869, 18.944732),
+            (50.226373, 18.944590)
+        ],
+        "Borek_Strzelinski": [
+            (50.226373, 18.944590),
+            (50.868446, 17.068697),
+            (50.867950, 17.070902),
+            (50.868710, 17.071801)
+        ],
+        "Dębki": [
+            (54.834099, 18.046263),
+            (54.817723, 18.047703),
+            (54.819077, 18.132574),
+            (54.836335, 18.130488)
+        ],
+        "Whitter": [
+            (60.788566, -148.817399),
+            (60.758982, -148.706227),
+            (60.757976, -148.632069),
+            (60.799709, -148.596020),
+            (60.795186, -148.826390)
+        ]
+    }
+}
+
+path_to_timeline_csv = "/Users/mikolajzawada/Documents/timeline.csv"
